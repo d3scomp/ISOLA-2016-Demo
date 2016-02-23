32f9ee83ec58a6a089eb989bcbc27e004d88aede
Vladimir Matena <matena@d3s.mff.cuni.cz>
2016-02-23 17:50:50 +0100

    Store runtime logs in configuration specific directory

--- src/cz/cuni/mff/d3s/isola2016/demo/DemoLauncher.java
+++ src/cz/cuni/mff/d3s/isola2016/demo/DemoLauncher.java
@@ -1,9 +1,12 @@
 package cz.cuni.mff.d3s.isola2016.demo;
 
+import java.util.Locale;
 import java.util.Random;
 
 import cz.cuni.mff.d3s.deeco.runners.DEECoSimulation;
 import cz.cuni.mff.d3s.deeco.runtime.DEECoNode;
+import cz.cuni.mff.d3s.deeco.runtimelog.RuntimeLogWriters;
+import cz.cuni.mff.d3s.deeco.runtimelog.RuntimeLogger;
 import cz.cuni.mff.d3s.deeco.simulation.omnet.OMNeTUtils;
 import cz.cuni.mff.d3s.isola2016.antsim.AntPlugin;
 import cz.cuni.mff.d3s.isola2016.antsim.AntWorldPlugin;
@@ -19,33 +22,40 @@ import cz.cuni.mff.d3s.jdeeco.position.PositionPlugin;
 import cz.cuni.mff.d3s.jdeeco.publishing.DefaultKnowledgePublisher;
 
 public class DemoLauncher {
-	public static final double RADIO_RANGE_M = 250; 
+	public static final double RADIO_RANGE_M = 250;
 	public static final int NUM_ANTS = 6;
 	public static final int NUM_FOOD_SOURCES = 4;
 	public static final int FOOD_SOURCE_CAPACITY = 3;
 	public static final double ANT_SPAWN_DIAMETER_M = 5;
 	public static final double FOOD_SOURCE_SPAWN_DIAMETER_M = 15;
 	public static final Position ANT_HILL_POS = new Position(0, 0);
+	public static final String LOG_PATH = "logs/runtime";
 
 	public static void main(String[] args) throws Exception {
-		run();
+		run(NUM_ANTS, NUM_FOOD_SOURCES, FOOD_SOURCE_CAPACITY, RADIO_RANGE_M);
 	}
-	
-	public static void run() throws Exception {
+
+	public static void run(int numAnts, int numFoodSources, int foodSourceCapacity, double radioRangeM)
+			throws Exception {
 		System.out.println("Ant food picking simulation demo");
 
+		// Setup logging directory
+		final String logPath = String.format(Locale.US, "%s_ants-%d_foods-%d_capacity-%d_range-%02f", LOG_PATH, numAnts,
+				numFoodSources, foodSourceCapacity, radioRangeM);
+		final RuntimeLogWriters logWriters = new RuntimeLogWriters(logPath);
+
 		OMNeTSimulation omnetSim = new OMNeTSimulation();
-		omnetSim.set80154txPower(OMNeTUtils.RangeToPower_802_15_4(RADIO_RANGE_M));
-		
+		omnetSim.set80154txPower(OMNeTUtils.RangeToPower_802_15_4(radioRangeM));
+
 		DEECoSimulation realm = new DEECoSimulation(omnetSim.getTimer());
-		
+
 		AntWorldPlugin antWorld = new AntWorldPlugin(ANT_HILL_POS);
 		Random rand = new Random(42);
-		
+
 		// Add food sources
-		for (int i = 0; i < NUM_FOOD_SOURCES; ++i) {
+		for (int i = 0; i < numFoodSources; ++i) {
 			Position pos = PosUtils.getRandomPosition(rand, 0, 0, FOOD_SOURCE_SPAWN_DIAMETER_M);
-			antWorld.addFoodSource(new FoodSource(pos, FOOD_SOURCE_CAPACITY));
+			antWorld.addFoodSource(new FoodSource(pos, foodSourceCapacity));
 		}
 
 		// Add plugins
@@ -59,8 +69,9 @@ public class DemoLauncher {
 		realm.addPlugin(IntelligentAntPlanning.class);
 
 		// Create nodes
-		for (int i = 0; i < NUM_ANTS; ++i) {
-			DEECoNode node = realm.createNode(i, new PositionPlugin(PosUtils.getRandomPosition(rand, 0, 0, ANT_SPAWN_DIAMETER_M)));
+		for (int i = 0; i < numAnts; ++i) {
+			DEECoNode node = realm.createNode(i, logWriters,
+					new PositionPlugin(PosUtils.getRandomPosition(rand, 0, 0, ANT_SPAWN_DIAMETER_M)));
 			node.deployComponent(new AntComponent(i, new Random(rand.nextLong()), node, ANT_HILL_POS));
 		}
 
@@ -68,10 +79,11 @@ public class DemoLauncher {
 		System.out.println("Running the simulation.");
 		realm.start(1500_000);
 		System.out.println("All done.");
-		
-		System.out.println("Total food pieced delivered: " + antWorld.collectedFoodPieces + " out of " + NUM_FOOD_SOURCES * FOOD_SOURCE_CAPACITY);
+
+		System.out.println("Total food pieced delivered: " + antWorld.collectedFoodPieces + " out of "
+				+ numFoodSources * foodSourceCapacity);
 		double totalDistance = 0;
-		for(AntPlugin ant: antWorld.ants) {
+		for (AntPlugin ant : antWorld.ants) {
 			System.out.println("Ant traveled distance: " + ant.totalTraveledDistance + " meters");
 			totalDistance += ant.totalTraveledDistance;
 		}
