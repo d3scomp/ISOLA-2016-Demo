0ff2dbc426bf758b69ca598d98dacf520e8e9d0c
Vladimir Matena <matena@d3s.mff.cuni.cz>
2016-04-23 23:12:19 +0200

    Update runner script

--- scripts/runner.py
+++ scripts/runner.py
@@ -6,7 +6,7 @@ MAX_THREADS = 48
 TOTAL_MEM_MB = 128000
 
 class Cfg(threading.Thread):
-    def __init__(self, seed, limit, maxtimeskew, numbigants, numsmallants, radiorange, rebroadcastrangem, rebrodcastdelay, networkModel):
+    def __init__(self, seed, limit, maxtimeskew, numbigants, numsmallants, radiorange, rebroadcastrangem, rebrodcastdelay, networkModel, mode, fitnessType):
         super().__init__()
         self.seed = seed
         self.limit = limit
@@ -18,11 +18,10 @@ class Cfg(threading.Thread):
         self.rebroadcastdelay = rebroadcastdelay
         self.userebroadcasting = True
         self.networkModel = networkModel
-        
-        self.running = False
+        self.mode = mode
+        self.fitnessType = fitnessType
         
     def run(self):
-        self.running = True
         print("Running execution thread")
         wd = os.getcwd()
         os.system("export PATH=$PATH" + os.pathsep + wd + os.sep + "omnet")
@@ -36,21 +35,21 @@ class Cfg(threading.Thread):
         args += '--rebroadcastDelayMs ' + str(self.rebroadcastdelay) + ' '
         args += '--useRebroadcasting ' + str(self.userebroadcasting) + ' '
         args += '--logIntervalMs 0 '
-        args += '--mode standard '
+        args += '--mode ' + str(self.mode) + ' '
         args += '--sourceCount 8 '
-#        args += '--perSourceRemoveProbabilityPerS 0.02 '
-        args += '--perSourceRemoveProbabilityPerS 0.1 '
+        args += '--perSourceRemoveProbabilityPerS 0.02 '
+#        args += '--perSourceRemoveProbabilityPerS 0.1 '
         args += '--networkModel ' + str(self.networkModel) + ' '
+        args += '--fitnessType ' + str(self.fitnessType) + ' '
 
 #        print(args)
         cmd = 'LD_LIBRARY_PATH=omnet java -Djava.library.path=/home/matena/ants/omnet -Xmx' + str(int(TOTAL_MEM_MB / MAX_THREADS)) + 'm -jar experiment.jar ' + args + ' > /dev/null 2>&1';
         print(cmd)
         os.system(cmd)
         print("Execution done")
-        self.running = False
         
 class MetaCfg:
-    def __init__(self, numbigantss, numsmallantss, radioranges, limits, seeds, rebroadcatDelays, rebroadcastRanges, maxtimeskews, networkModels, modes):
+    def __init__(self, numbigantss, numsmallantss, radioranges, limits, seeds, rebroadcatDelays, rebroadcastRanges, maxtimeskews, networkModels, modes, fitnessTypes):
         self.numbigantss = numbigantss
         self.numsmallantss = numsmallantss
         self.radioranges = radioranges
@@ -61,24 +60,26 @@ class MetaCfg:
         self.maxtimeskews = maxtimeskews
         self.networkModels = networkModels
         self.modes = modes
+        self.fitnessTypes = fitnessTypes
 
 metaCfgs = [];
 cfgs = [];
 
 # Define meta configurations
-for mode in ['standard']:#['standard', 'quantum']:
+for mode in ['quantum']:#['standard', 'quantum']:
     for networkModel in ['simple']:#['simple', 'omnet']:
         metaCfgs.append(MetaCfg(
                     numbigantss = [6],
                     numsmallantss = [40],
-                    radioranges = [5, 7],
+                    radioranges = [3, 5, 7],
                     limits = [1800000],
-                    seeds = range(0, 10),
+                    seeds = range(0, 30),
                     rebroadcatDelays = [1000, 5000, 10000, 15000, 30000],
                     rebroadcastRanges = [0, 5, 10, 15],
                     maxtimeskews = [1000, 5000, 10000, 30000, 60000],
                     networkModels = [networkModel],
-                    modes = [mode]
+                    modes = [mode],
+                    fitnessTypes = ["PreferClose", "PreferDistant"]
                     ))
 
 print("Generating using " + str(len(metaCfgs)) + " meta configurations")
@@ -94,17 +95,20 @@ for metaCfg in metaCfgs:
                                 for maxtimeskew in metaCfg.maxtimeskews:
                                     for networkModel in metaCfg.networkModels:
                                         for mode in metaCfg.modes:
-                                            cfgs.append(Cfg(
-                                                numbigants=numbigants,
-                                                numsmallants=numsmallants,
-                                                seed=seed,
-                                                limit=limit,
-                                                maxtimeskew=maxtimeskew,
-                                                radiorange=radiorange,
-                                                rebroadcastrangem=rebroadcastrange,
-                                                rebrodcastdelay=rebroadcastdelay,
-                                                networkModel=networkModel)
-                                            )
+                                            for fitnessType in metaCfg.fitnessTypes:
+                                                cfgs.append(Cfg(
+                                                    numbigants=numbigants,
+                                                    numsmallants=numsmallants,
+                                                    seed=seed,
+                                                    limit=limit,
+                                                    maxtimeskew=maxtimeskew,
+                                                    radiorange=radiorange,
+                                                    rebroadcastrangem=rebroadcastrange,
+                                                    rebrodcastdelay=rebroadcastdelay,
+                                                    networkModel=networkModel,
+                                                    mode = mode,
+                                                    fitnessType = fitnessType)
+                                                )
     
 print("Running " + str(len(cfgs)) + " configurations")
 
@@ -113,7 +117,7 @@ running = []
 while len(cfgs) > 0:
     active = 0;
     for cfg in running:
-        if cfg.running == True:
+        if cfg.isAlive() == True:
             active = active + 1
     print("Active: " + str(active))
     if active >= MAX_THREADS:
