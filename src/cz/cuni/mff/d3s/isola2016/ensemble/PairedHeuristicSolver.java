2c974241197658df576546e3e6530e70ad4fb86d
Vladimir Matena <matena@d3s.mff.cuni.cz>
2016-04-26 10:31:35 +0200

    Remove debug print

--- src/cz/cuni/mff/d3s/isola2016/ensemble/PairedHeuristicSolver.java
+++ src/cz/cuni/mff/d3s/isola2016/ensemble/PairedHeuristicSolver.java
@@ -99,7 +99,6 @@ public class PairedHeuristicSolver implements AntAssignmetSolver {
 					throw new UnsupportedOperationException("Fitness calculation not defined for mode: " + mode);
 				}
 			}
-			System.err.println(sourceFitnessCache);
 			return sourceFitnessCache;
 		}
 
