94896cbadf0742375d50bc74cf240be8a23b8876
Vladimir Matena <matena@d3s.mff.cuni.cz>
2016-04-26 09:42:06 +0200

    Updated scripts

--- scripts/comparator.py
+++ scripts/comparator.py
@@ -1,6 +1,5 @@
 import simloader
 import os
-import sys
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plot
@@ -9,65 +8,16 @@ import numpy as np
 
 print("Comparator generic dimensions")
 
-# logsDir = "../logs/10-99rebprob"
+#logsDir = "../logs/10-99rebprob"
 logsDir = "logs"
 
-def cfgGuard(log, dim):
-    if not hasattr(log.config, "mode"):
-        log.config.mode = "standard"
-    if not hasattr(log.config, "networkModel"):
-        log.config.networkModel = "simple"
-        
-    if log.config.mode != dim['mode']:
-        return False
-    if float(log.config.radioRangeM) != float(dim['radiorange']):
-        #print(str(log.config.radioRangeM) + " != " + str(dim['radiorange']))
-        return False
-    if log.config.networkModel != dim['networkModel']:
-        #print(log.config.networkModel + " != " + dim['networkModel'])
-        return False
-    
-    return True
+dimensions = {
+              "rebroadcastRangeM": lambda log: log.config.rebroadcastDelayMs < 5000,
+              "rebroadcastDelayMs": lambda log: log.config.rebroadcastRangeM >= 5 and log.config.maxTimeSkewMs > 10000,
+              "maxTimeSkewMs": lambda log: log.config.rebroadcastRangeM > 5 and log.config.rebroadcastDelayMs > 5000
+             }
 
-dimensions = []
-for networkModel in ['simple']:#['simple', 'omnet']:
-    for mode in ['quantum']:#['standard', 'quantum']:
-        for fitness in ["PreferClose", "PreferDistant", "PreferNeutral"]:
-            for radiorange in [5]:#[3, 5, 7]:
-                dimensions.append({
-                'headline': fitness + " - rebroadcast range on " + mode + " " + str(radiorange) + "m radio range (" + networkModel + ")",
-                'xaxisText': "Rebroadcast range in meters",
-                'xaxisTransform': lambda val: str(val),
-                'value': "rebroadcastRangeM",
-                'mode': mode,
-                'networkModel': networkModel,
-                'radiorange': radiorange,
-                'filter': lambda log, dim: cfgGuard(log, dim) and log.config.rebroadcastDelayMs == 5000 and log.config.maxTimeSkewMs == 5000,
-                })
-                
-                dimensions.append({
-                'headline': fitness + " - rebroadcast delay on " + mode + " " + str(radiorange) + "m radio range (" + networkModel + ")",
-                'xaxisText': "Rebroadcast period in seconds",
-                'xaxisTransform': lambda val: str(val / 1000),
-                'value': "rebroadcastDelayMs",
-                'mode': mode,
-                'networkModel': networkModel,
-                'radiorange': radiorange,
-                'filter': lambda log, dim: cfgGuard(log, dim) and log.config.rebroadcastRangeM == 10 and log.config.maxTimeSkewMs == 30000
-                })
-                
-                dimensions.append({
-                'headline': fitness + " - old knowledge removal on " + mode + " " + str(radiorange) + "m radio range (" + networkModel + ")",
-                'xaxisText': "Maximal allowed knowledge age in seconds",
-                'xaxisTransform': lambda val: str(val / 1000),
-                'value': "maxTimeSkewMs",
-                'mode': mode,
-                'networkModel': networkModel,
-                'radiorange': radiorange,
-                'filter': lambda log, dim: cfgGuard(log, dim) and log.config.rebroadcastRangeM == 10 and log.config.rebroadcastDelayMs == 10000
-                })
-
-def boxplot(data, msgdata, name="comparison", xaxisText="value", xaxisTransform=lambda x: x):
+def boxplot(data, msgdata, name = "comparison"):
     print ("NAME " + name)
     pdata = []
     msgpdata = []
@@ -75,14 +25,14 @@ def boxplot(data, msgdata, name="comparison", xaxisText="value", xaxisTransform=
     plot.close()
     
     # Boxplot names and data aggregation
-    cnt = 0
+    cnt = 1
     xtckcnt = []
     xtckname = []
     for key in sorted(data.keys()):
         pdata.append(data[key])
         xtckcnt.append(cnt)
         cnt = cnt + 2
-        xtckname.append(str(xaxisTransform(key)))
+        xtckname.append(str(key))
         
     for key in sorted(msgdata.keys()):
         msgpdata.append(msgdata[key])
@@ -94,94 +44,66 @@ def boxplot(data, msgdata, name="comparison", xaxisText="value", xaxisTransform=
     
     # Box-plots
    
-    databoxes = ax1.boxplot(pdata, positions=range(0, len(pdata) * 2, 2), labels=xtckname)
+    databoxes = ax1.boxplot(pdata, positions = range(0,len(pdata) * 2, 2), labels=xtckname)
     plot.setp(databoxes['boxes'], color='blue')
     
-    msgboxes = ax2.boxplot(msgpdata, positions=range(1, len(msgpdata) * 2 + 1, 2))
+    msgboxes = ax2.boxplot(msgpdata, positions = range(1,len(msgpdata) * 2 + 1, 2))
     plot.setp(msgboxes['boxes'], color='green')
     
-    plot.xlim(-1, len(pdata) * 2)
+    plot.xlim(-2, len(pdata) * 2 + 1)
     
     # value dots
     for i in range(len(pdata)):
         y = pdata[i]
-        x = 2 * i;
+        x = 2*i;
         px = np.random.normal(x, 0.075, size=len(y))
         ax1.plot(px, y, 'bo', alpha=0.4)
-        
-    # msg dots
-    for i in range(len(pdata)):
-        y = msgpdata[i]
-        x = 2 * i + 1;
-        px = np.random.normal(x, 0.075, size=len(y))
-        ax2.plot(px, y, 'go', alpha=0.4)
     
-    plot.title(name)
     plot.xticks(xtckcnt, xtckname)
-    ax1.set_xlabel(xaxisText)
-    ax1.set_ylabel("Solution value in collected foods", color="blue")
-    ax2.set_ylabel("Number of messages", color="green")
-    plot.savefig(name + ".png", dpi=256, width=20, wight=15)
+    plot.xlabel(name)
+    ax1.set_ylabel("Solution value in collected foods", color = "blue")
+    ax2.set_ylabel("Number of messages", color = "green")
+    plot.savefig(name + ".png", dpi=256, width = 20, wight = 15)
 
-def loadAllLogs():
-    logs = []
+def processDimension(dimension, filter):
+    data = {}
+    msgdata = {}
     for (dirpath, dirnames, filenames) in os.walk(logsDir):
         for dir in dirnames:
             if dir.startswith("world"):
                 path = dirpath + os.sep + dir;
-                #print("Path: " + path)
-                                
+                print("Path: " + path)
+                
                 if not os.path.isfile(path + os.sep + "final.xml"):
-                    print("!", end='', flush=True)
+                    print("no final.xml in path, skipping")
                     continue
                             
-                logs.append(simloader.loadFinal(path))
-    return logs
-
-def processDimension(dimension, logs):
-    data = {}
-    msgdata = {}
-    
-    for log in logs:
-        # Custom filtering
-        if dimension["filter"](log, dimension) == False:
-            print("-", end='', flush=True)
-            continue
-        
-        print("+", end='', flush=True)
-        
-        key = log.config[dimension['value']];
-        collected = log.report.collected
-        utility = log.report.utility
-        messages = log.report.msgCounter.sendCounter
-        
-        # Add collected record to data to process      
-        if not key in data:
-            data[key] = []
-            msgdata[key] = []
-        data[key].append(int(utility))
-        msgdata[key].append(int(messages))
-
-    print("")
+                log = simloader.loadFinal(path)
+                
+                # Custom filtering
+                if not dimensions[dimension](log):
+                    print("FILTERED OUT")
+                    continue
+                
+                key = log.config[dimension];
+                collected = log.report.collected
+                messages = log.report.numMessages
+                
+                # Add collected record to data to process      
+                if not key in data:
+                    data[key] = []
+                    msgdata[key] = []
+                data[key].append(int(collected))
+                msgdata[key].append(int(messages))
+        break
 
     print("Data: ")
     for key in data:
         print(str(key) + ": " + str(data[key]) + " " + str(msgdata[key]))
     print("Data end")
     
-    if len(data) > 0:
-        boxplot(data, msgdata, name=dimension['headline'], xaxisText=dimension['xaxisText'], xaxisTransform=dimension['xaxisTransform'])
-    else:
-        print("No data for this comparison dimension")
-        print(dimension['headline'])
-
-if len(sys.argv) == 2:
-    logsDir = sys.argv[1];
+    boxplot(data, msgdata, name = dimension)
     
-print("Using logsdir: " + logsDir);
-
-logs = loadAllLogs()
-
 for dimension in dimensions:
-    processDimension(dimension, logs)
+    processDimension(dimension, dimensions[dimension])
 
--- scripts/runner.py
+++ scripts/runner.py
@@ -2,11 +2,10 @@ import os
 import threading
 from time import sleep
 
-MAX_THREADS = 48
-TOTAL_MEM_MB = 128000
+os.chdir("..")
 
 class Cfg(threading.Thread):
-    def __init__(self, seed, limit, maxtimeskew, numbigants, numsmallants, radiorange, rebroadcastrangem, rebrodcastdelay, networkModel, mode, fitnessType):
+    def __init__(self, seed, limit, maxtimeskew, numbigants, numsmallants, radiorange):
         super().__init__()
         self.seed = seed
         self.limit = limit
@@ -14,148 +13,44 @@ class Cfg(threading.Thread):
         self.numbigants = numbigants
         self.numsmallants = numsmallants
         self.radiorange = radiorange
-        self.rebroadcastrangem = rebroadcastrangem
-        self.rebroadcastdelay = rebroadcastdelay
-        self.userebroadcasting = True
-        self.networkModel = networkModel
-        self.mode = mode
-        self.fitnessType = fitnessType
         
     def run(self):
         print("Running execution thread")
         wd = os.getcwd()
         os.system("export PATH=$PATH" + os.pathsep + wd + os.sep + "omnet")
-        args = '--numBigAnts ' + str(self.numbigants) + ' '
-        args += '--numSmallAnts ' + str(self.numsmallants) + ' '
+        args = '--numbigants ' + str(self.numbigants) + ' '
+        args += '--numsmallants ' + str(self.numsmallants) + ' '
         args += '--seed ' + str(self.seed) + ' '
-        args += '--limitMs ' + str(self.limit) + ' '
-        args += '--maxTimeSkewMs ' + str(self.maxtimeskew) + ' '
-        args += '--radioRangeM ' + str(self.radiorange) + ' '        
-        args += '--rebroadcastRangeM ' + str(self.rebroadcastrangem) + ' '
-        args += '--rebroadcastDelayMs ' + str(self.rebroadcastdelay) + ' '
-        args += '--useRebroadcasting ' + str(self.userebroadcasting) + ' '
-        args += '--logIntervalMs 0 '
-        args += '--mode ' + str(self.mode) + ' '
-        args += '--sourceCount 8 '
-        args += '--perSourceRemoveProbabilityPerS 0.02 '
-#        args += '--perSourceRemoveProbabilityPerS 0.1 '
-        args += '--networkModel ' + str(self.networkModel) + ' '
-        args += '--fitnessType ' + str(self.fitnessType) + ' '
-
-#        print(args)
-        cmd = 'LD_LIBRARY_PATH=omnet java -Djava.library.path=/home/matena/ants/omnet -Xmx' + str(int(TOTAL_MEM_MB / MAX_THREADS)) + 'm -jar experiment.jar ' + args + ' > /dev/null 2>&1';
-        print(cmd)
-        os.system(cmd)
+        args += '--limit ' + str(self.limit) + ' '
+        args += '--maxtimeskew ' + str(self.maxtimeskew) + ' '
+        args += '--radiorange ' + str(self.radiorange) + ' '        
+        os.system('java -jar experiment.jar ' + args)
         print("Execution done")
-        
-class MetaCfg:
-    def __init__(self, numbigantss, numsmallantss, radioranges, limits, seeds, rebroadcatDelays, rebroadcastRanges, maxtimeskews, networkModels, modes, fitnessTypes):
-        self.numbigantss = numbigantss
-        self.numsmallantss = numsmallantss
-        self.radioranges = radioranges
-        self.limits = limits
-        self.seeds = seeds
-        self.rebroadcatDelays = rebroadcatDelays
-        self.rebroadcastRanges = rebroadcastRanges
-        self.maxtimeskews = maxtimeskews
-        self.networkModels = networkModels
-        self.modes = modes
-        self.fitnessTypes = fitnessTypes
 
-metaCfgs = [];
+MAX_THREADS = 42
 cfgs = [];
-limit_ms = 1800000
 
-# Define meta configurations
-for mode in ['quantum']:#['standard', 'quantum']:
-    for networkModel in ['simple']:#['simple', 'omnet']:
-        # Rebroadcast range comparison
-        metaCfgs.append(MetaCfg(
-                    numbigantss = [6],
-                    numsmallantss = [40],
-                    radioranges = [5],#[3, 5, 7],
-                    limits = [limit_ms],
-                    seeds = range(0, 10),
-                    rebroadcatDelays = [5000],
-                    rebroadcastRanges = [0, 5, 10, 15],
-                    maxtimeskews = [5000],
-                    networkModels = [networkModel],
-                    modes = [mode],
-                    fitnessTypes = ["PreferClose", "PreferDistant", "PreferNeutral"]
-                    ))
-        # Rebroadcast delay
-        metaCfgs.append(MetaCfg(
-                    numbigantss = [6],
-                    numsmallantss = [40],
-                    radioranges = [5],#[3, 5, 7],
-                    limits = [limit_ms],
-                    seeds = range(0, 10),
-                    rebroadcatDelays = [1000, 5000, 10000, 15000],
-                    rebroadcastRanges = [10],
-                    maxtimeskews = [30000],
-                    networkModels = [networkModel],
-                    modes = [mode],
-                    fitnessTypes = ["PreferClose", "PreferDistant", "PreferNeutral"]
-                    ))
-        # Knowledge age preference 
-        metaCfgs.append(MetaCfg(
-                    numbigantss = [6],
-                    numsmallantss = [40],
-                    radioranges = [5],#[3, 5, 7],
-                    limits = [limit_ms],
-                    seeds = range(0, 10),
-                    rebroadcatDelays = [10000],
-                    rebroadcastRanges = [10],
-                    maxtimeskews = [1000, 5000, 10000, 30000, 60000],
-                    networkModels = [networkModel],
-                    modes = [mode],
-                    fitnessTypes = ["PreferClose", "PreferDistant", "PreferNeutral"]
-                    ))
-
-print("Generating using " + str(len(metaCfgs)) + " meta configurations")
-
-for metaCfg in metaCfgs:
-    for numbigants in metaCfg.numbigantss:
-        for numsmallants in metaCfg.numsmallantss:
-            for radiorange in metaCfg.radioranges:
-                for limit in metaCfg.limits:
-                    for seed in metaCfg.seeds:
-                        for rebroadcastdelay in metaCfg.rebroadcatDelays:
-                            for rebroadcastrange in metaCfg.rebroadcastRanges:
-                                for maxtimeskew in metaCfg.maxtimeskews:
-                                    for networkModel in metaCfg.networkModels:
-                                        for mode in metaCfg.modes:
-                                            for fitnessType in metaCfg.fitnessTypes:
-                                                cfgs.append(Cfg(
-                                                    numbigants=numbigants,
-                                                    numsmallants=numsmallants,
-                                                    seed=seed,
-                                                    limit=limit,
-                                                    maxtimeskew=maxtimeskew,
-                                                    radiorange=radiorange,
-                                                    rebroadcastrangem=rebroadcastrange,
-                                                    rebrodcastdelay=rebroadcastdelay,
-                                                    networkModel=networkModel,
-                                                    mode = mode,
-                                                    fitnessType = fitnessType)
-                                                )
+# Define configurations
+numbigants = 10
+numsmallants = 40
+radiorange = 5
+limit = 300000
+seeds = range(0, 20)
+maxtimeskews = [500, 1000, 2000, 3000, 5000, 10000, 30000, 10000000]
+for seed in seeds:
+    for maxtimeskew in maxtimeskews:
+        cfgs.append(Cfg(numbigants=numbigants, numsmallants=numsmallants, seed=seed, limit=limit, maxtimeskew=maxtimeskew, radiorange=radiorange))
     
 print("Running " + str(len(cfgs)) + " configurations")
 
 running = []
 
 while len(cfgs) > 0:
-    active = 0;
-    for cfg in running:
-        if cfg.isAlive() == True:
-            active = active + 1
-    print("Active: " + str(active))
-    if active >= MAX_THREADS:
-        sleep(1)
-        continue
+    if threading.active_count() >= MAX_THREADS:
+        running.pop().join();
     
     print("To go: " + str(len(cfgs)))
-    sleep(1);
+    sleep(3);
     cfg = cfgs.pop()
     running.append(cfg)
     cfg.start()
